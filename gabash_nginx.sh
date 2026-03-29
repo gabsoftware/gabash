5937fa4e7fbb6101e521cd35c3461c28bf7fd223
Gabriel Hautclocq <gabrielhautclocq@gmail.com>
2026-03-29 23:05:24 +0200

    improved ngr and added ngs (nginx status)

--- gabash_nginx.sh
+++ gabash_nginx.sh
@@ -7,10 +7,43 @@ unset __GABASH_SCRIPT_DIR
 
 #### shortcuts for working with nginx
 
+function __nginx_has_service() {
+  [ -x /usr/sbin/service ]
+}
+
+function __nginx_has_systemctl() {
+  [ -x /usr/bin/systemctl ]
+}
+
 function ngr() {
-  sudo service nginx restart
+  if __nginx_has_service; then
+    if sudo service nginx status > /dev/null 2>&1; then
+      sudo service nginx stop
+    fi
+    sudo service nginx start
+  elif __nginx_has_systemctl; then
+    if sudo systemctl is-active --quiet nginx; then
+      sudo systemctl stop nginx
+    fi
+    sudo systemctl start nginx
+  else
+    echo "ngr: neither service nor systemctl found"
+    return 1
+  fi
 }
 
 function ngt() {
   sudo nginx -t
 }
+
+function ngs() {
+  if __nginx_has_service; then
+    sudo service nginx status
+  elif __nginx_has_systemctl; then
+    sudo systemctl status nginx
+  else
+    echo "ngs: neither service nor systemctl found"
+    return 1
+  fi
+}
+
